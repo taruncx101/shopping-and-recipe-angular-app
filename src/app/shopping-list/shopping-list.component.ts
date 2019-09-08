f0d0ff78f39ff5e7be6900adb2515b9efd3ee5d2
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-08 17:26:16 +0530

    ingredientAdded

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,7 +15,6 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   onIngredientAdded(ingredient: Ingredient) {
-    console.log(125)
     this.ingredients.push(ingredient);
   }
   ngOnInit() {
