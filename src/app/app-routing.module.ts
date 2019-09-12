3bdebce9aab48cc8d079a071d8f4796a6e2d4c1b
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-12 22:43:13 +0530

    adding recipe edit componet & routes for add and edit recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -9,7 +10,9 @@ const appRoutes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     {path: '', component: RecipeStartComponent},
+    {path: 'new', component: RecipeEditComponent},
     {path: ':id', component: RecipeDetailComponent},
+    {path: ':id/edit', component: RecipeEditComponent},
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
