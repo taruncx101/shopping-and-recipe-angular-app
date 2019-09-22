6a0a88c9bdaed1d8a7f8eb22c3beee1df597c250
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-22 13:40:35 +0530

    redirect after login or signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { AuthService, AuthResponseData } from './auth.service';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,8 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -39,6 +41,7 @@ export class AuthComponent implements OnInit {
     }
     authObs.subscribe( resData => {
       this.isLoading = false;
+      this.router.navigate(['/recipes']);
       console.log(resData);
     }, errorMsg => {
       this.isLoading = false;
